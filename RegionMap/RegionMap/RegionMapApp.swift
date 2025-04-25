93f086ee94b7712c00a1733e155de2d8dee73078
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-04-25 08:15:28 +0300

    Add files via upload

---
+++ RegionMap/RegionMap/RegionMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegionMapApp.swift
+//  RegionMap
+//
+//  Created by Berkan Aydın on 25.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct RegionMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
